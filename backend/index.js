03768797bafbfafb01bb4eed47b8ac24a4a1cf44
Gauravthkaur <gaurav079t@gmail.com>
2025-02-19 22:54:24 +0530

    new index

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 const corsOptions = {
-  origin: 'https://employee-management-system-theta-lac.vercel.app'|| process.env.FRONTEND_URL,
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
